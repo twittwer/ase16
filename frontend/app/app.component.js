103c33e2a10f6bf283b70504b2788a0299aeb293
Tobias Wittwer <t.wittwer95@gmail.com>
2016-10-18 09:57:05 +0200

    Frontend

--- frontend/app/app.component.js
+++ frontend/app/app.component.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>Frontend - It works!</h1><h3>Powered by Angular2</h3>'
+            template: '<h1>Frontend - It works!</h1><h3>- Powered by Angular2</h3>'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>Frontend - It works!</h1><h3>Powered by Angular2</h3>'
+  template: '<h1>Frontend - It works!</h1><h3>- Powered by Angular2</h3>'
 })
 export class AppComponent { }
 
