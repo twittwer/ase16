089fb4c42c658b236d26cd371340c845b1ed1afd
Tobias Wittwer <t.wittwer95@gmail.com>
2016-10-21 11:49:55 +0200

    Fix merge

--- frontend/app/app.module.ts
+++ frontend/app/app.module.ts
@@ -1,17 +1,11 @@
-import {NgModule}      from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
-
-<<<<<<< HEAD
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { AppComponent }  from './app.component';
 import { HeaderComponent }  from './header.component';
 import { MessageBoxComponent }  from './messagebox.component';
 import { NgbdModalBasic } from './modal-login.component';
-import { VotingComponent } from './voting.component';
-import { VotingFormComponent }  from './voting-Form.component';
-
-import { Cookie } from 'ng2-cookies/ng2-cookies';
-
+import { UserService } from "./user.service";
 
 @NgModule({
   imports: [
@@ -22,32 +16,10 @@ import { Cookie } from 'ng2-cookies/ng2-cookies';
     AppComponent,
     HeaderComponent,
     MessageBoxComponent,
-    NgbdModalBasic,
-    VotingComponent,
-    VotingFormComponent
+    NgbdModalBasic
   ],
+  providers: [ UserService ],
   bootstrap: [ AppComponent ]
-=======
-import {AppComponent}  from './app.component';
-import {HeaderComponent}  from './header.component';
-import {MessageBoxComponent}  from './messagebox.component';
-import {NgbdModalBasic} from './modal-login.component';
-import {UserService} from "./user.service";
-
-@NgModule({
-    imports: [
-        BrowserModule,
-        NgbModule.forRoot()
-    ],
-    declarations: [
-        AppComponent,
-        HeaderComponent,
-        MessageBoxComponent,
-        NgbdModalBasic
-    ],
-    providers: [UserService],
-    bootstrap: [AppComponent]
->>>>>>> bcfb52f0381d5c220ac0d4bb1378a5719c286c15
 })
 export class AppModule {
 }
