4ca7d26f73ce60d64400158f68ad393c0ff95c18
Sarah Ruess <sarah.ruess@hpe.com>
2016-10-22 12:33:32 +0200

    send vote

--- frontend/app/voting.component.ts
+++ frontend/app/voting.component.ts
@@ -1,34 +1,60 @@
 import { Component } from '@angular/core';
 import {VoteService} from './vote.service';
 
+
 @Component({
   selector: 'voting',
   template: `
     <div class="panel panel-default">
-      <div class="panel-body" #myPanel>
-        <div *ngFor="let option of voteOptions" class="input-group input-group-lg">
-            <label class="checkbox-inline">
-              <input type="checkbox" id={{option.key}} value={{option.title}} >{{option.title}}
+      <div class="panel-body" >
+        <div *ngFor="let option of voteOptions">
+            <label >
+              <input type="checkbox"  value="{{option.title}}" name="options" [checked]="voteOptions.indexOf(option.title) >= 0" (change)="updateCheckedOption(option.title, $event)">{{option.title}}
             </label>
         </div>
-        <button class="btn btn-default" type="button" (click)="sendVote(myPanel)">Vote</button>
+        <button class="btn btn-default" type="button" (click)="sendVote()">Vote</button>
       </div>
     </div>
-  `})
+  `
+})
 export class VotingComponent {
   constructor(private voteservice:VoteService){};
   private currentVote = this.voteservice.getCurrentVote();
   public voteOptions = this.currentVote.options;
+  private optionsMap: any[] =  [];
+  public optionsChecked: any[] = [];
+  initOptionsMap();
 
-
-
-  sendVote(parent: any) {
-    console.log("test", parent);
-    /*
-    let elements = parent.children;
-    for(let i = 0;i<element.length-2;i++) {
-      elements[i]
+  initOptionsMap(){
+    for (var x:any = 0; x<this.voteOptions.length; x++) {
+      this.optionsMap.push([this.voteOptions[x].title, true]);
+        // this.optionsMap[this.voteOptions[x].title] = true;
     }
-    */
+  }
+
+  updateOptions() {
+      for(var x in this.optionsMap) {
+          if(this.optionsMap[x]) {
+            console.log("push?");
+              this.optionsChecked.push(x);
+          }
+      }
+      this.optionsChecked = [];
+  }
+
+  updateCheckedOption(option, event) {
+    for (var x:any = 0; x<this.voteOptions.length; x++) {
+        this.optionsMap[this.voteOptions[x].title] = true;
+    }
+   this.updateOptions();
+ }
+
+
+
+  sendVote() {
+    this.voteservice.sendOpinion(this.optionsMap, (success: any)=>{
+      console.log(success);
+    });
+
   }
 }
